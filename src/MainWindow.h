aa8f1712d8d21a802b007c4ce47960443c653645
Alex <158708149+alexandreded@users.noreply.github.com>
2024-11-08 17:47:33 +0300

    Update MainWindow.h

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -10,6 +10,7 @@
 #include <qwt_plot_curve.h>
 #include <QTimer>
 #include <QVector>
+#include <memory>
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -23,27 +24,34 @@ private slots:
     void updatePlots();
 
 private:
-    AdaptiveUnscentedKalmanFilter *filter;
-    BeamSimulation *simulation;
+    std::unique_ptr<AdaptiveUnscentedKalmanFilter> filter;
+    std::unique_ptr<BeamSimulation> simulation;
 
     QTimer *timer;
 
-    // Графики
+    // Графики интенсивностей
     QwtPlot *intensityPlot;
     QwtPlotCurve *curveIA;
     QwtPlotCurve *curveIB;
     QwtPlotCurve *curveIC;
     QwtPlotCurve *curveID;
 
+    // Графики состояний
     QwtPlot *statePlot;
     QwtPlotCurve *curveState1;
     QwtPlotCurve *curveState2;
     QwtPlotCurve *curveState3;
     QwtPlotCurve *curveState4;
 
+    // График координат
+    QwtPlot *coordinatePlot;
+    QwtPlotCurve *curveRealX;
+    QwtPlotCurve *curveEstimatedX;
+
     QVector<double> timeData;
     QVector<double> IAData, IBData, ICData, IDData;
     QVector<double> state1Data, state2Data, state3Data, state4Data;
+    QVector<double> realXData, estimatedXData;
 
     int currentTime;
 };
